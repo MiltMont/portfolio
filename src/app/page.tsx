633c3c44abf49ec8da5b24802acc670b2ea0e602
MiltMont <miltgonmont@pm.me>
2024-07-02 02:34:06 -0600

    chore: Add HomeHero component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,8 @@
 import Image from "next/image";
 import { Gutter } from "../components/Gutter";
 import classes from "./classes.module.scss";
+import { HomeHero } from "../components/Hero/Home";
 
 export default function Home() {
-  return (
-    <Gutter>
-      <h1>Welcome!</h1>
-      <div className="grid">
-        <h1
-          className={["start-1 cols-8 start-m-1 cols-m-2", classes.col1].join(
-            " "
-          )}
-        >
-          A
-        </h1>
-        <h1
-          className={["start-9 cols-8 start-m-5 cols-m-2", classes.col2].join(
-            " "
-          )}
-        >
-          B
-        </h1>
-
-        <h1 className={["start-1 cols-16", classes.col3].join(" ")}>C</h1>
-      </div>
-    </Gutter>
-  );
+  return <HomeHero />;
 }
