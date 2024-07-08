59033a38ebfe6f5cbdaa6ef739946c492f19d424
MiltMont <miltgonmont@pm.me>
2024-07-08 10:56:37 -0600

    refactor: Add @faceless-ui/scroll-info dependency and update MobileNavigation component to update background opacity based on scroll information.

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DesktopNavigation } from "./DesktopNavigation";
 import {
   MobileNavigation,
@@ -10,17 +10,32 @@ import { useModal } from "@faceless-ui/modal";
 import { MainMenu } from "@types";
 
 import classes from "./classes.module.scss";
+import { useScrollInfo } from "@faceless-ui/scroll-info";
 
 export const Header: React.FC<MainMenu> = ({ tabs }) => {
   const { isModalOpen } = useModal();
   const isMobileNavigationOpen = isModalOpen(mobileNavigationModalSlug);
+  const { y } = useScrollInfo();
+  const [hideBackground, setHideBackground] = useState(true);
+
+  useEffect(() => {
+    if (isMobileNavigationOpen) {
+      setHideBackground(false);
+    } else {
+      setHideBackground(y < 30);
+    }
+  }, [y, isMobileNavigationOpen]);
+
+  console.log(hideBackground);
 
   return (
     <div>
       <header
         className={[
           classes.header,
+          classes.headerTheme,
           isMobileNavigationOpen && classes.mobileNavigationOpen,
+          hideBackground && classes.hideBackground,
         ]
           .filter(Boolean)
           .join(" ")}
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -3,16 +3,19 @@
 import React from "react";
 import { ThemePreferenceProvider } from "./Theme";
 import { ModalProvider, ModalContainer } from "@faceless-ui/modal";
+import { ScrollInfoProvider } from "@faceless-ui/scroll-info";
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
     <ThemePreferenceProvider>
-      <ModalProvider transTime={0} zIndex="var(--z-modal)">
-        {children}
-        <ModalContainer />
-      </ModalProvider>
+      <ScrollInfoProvider>
+        <ModalProvider transTime={0} zIndex="var(--z-modal)">
+          {children}
+          <ModalContainer />
+        </ModalProvider>
+      </ScrollInfoProvider>
     </ThemePreferenceProvider>
   );
 };
