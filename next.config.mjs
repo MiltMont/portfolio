67fce77415dec7bfb22ba61231f3ba948002820f
MiltMont <miltgonmont@pm.me>
2024-07-13 19:22:00 -0600

    chore: Update next.config.mjs with new remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         remotePatterns: [
             {
                 hostname: "localhost"
+            }, 
+            {
+                hostname: "portfolio-cms-zmpgeoiema-uc.a.run.app"
             }
         ]
     }
