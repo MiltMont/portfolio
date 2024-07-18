3bbc708ba319796c0ebc03534c90461af0c31511
MiltMont <miltgonmont@pm.me>
2024-07-18 10:28:35 -0600

    fix: Add bottom margin to post container.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,21 @@
+import MinimalHero from "@components/Hero/Minimal";
 import DisplayPosts from "@layout/DisplayPosts";
 import { fetchBlogPosts } from "@root/graphql";
+
+import classes from "./classes.module.scss";
 
 const Blog = async () => {
   const blogPosts = await fetchBlogPosts();
   return (
-    <>
+    <div className={classes.container}>
+      <MinimalHero
+        content={"Read all my posts"}
+        description={
+          "I try to write as much as posible, it usually helps me to understand a subject better."
+        }
+      />
       <DisplayPosts posts={blogPosts} />
-    </>
+    </div>
   );
 };
 
