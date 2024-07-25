6ae57810f66b4dffbf6b60f648d608eb2f6fca8c
MiltMont <miltgonmont@pm.me>
2024-07-24 19:32:20 -0600

    feat: Temporal modification to CTA component.

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { Gutter } from "@components/Gutter";
 import classes from "./classes.module.scss";
+import { ArrowLeft, ArrowRight, ArrowUpRight } from "iconoir-react";
+import Link from "next/link";
 
 export const CallToAction: React.FC<any> = ({}) => {
   return (
-    <Gutter className={classes.ctaWrapper}>
-      <div className={[classes.ctaContainer, "grid"].filter(Boolean).join(" ")}>
-        <div
-          className={[classes.content, "start-1 cols-8 start-m-1 cols-m-4"]
-            .filter(Boolean)
-            .join(" ")}
-        >
-          Read all my posts
-        </div>
-        <div
-          className={[classes.actions, "start-13 cols-4 start-m-5 cols-4"]
-            .filter(Boolean)
-            .join(" ")}
-        >
-          &rarr;
+    <Gutter>
+      <div className={classes.ctaContainer}>
+        <div className={classes.content}>Feel free to read all of my posts</div>
+        <div className={classes.links}>
+          <Link
+            href={"/blog"}
+            className={[classes.link, "cols-4"].filter(Boolean).join(" ")}
+          >
+            <div className={classes.linkLabel}>Check them out</div>
+            <ArrowUpRight />
+          </Link>
         </div>
       </div>
     </Gutter>
